2d1b3804d352cf003eeaf377e5a005da219874fb
altar horowitz <altarh@mail.tau.ac.il>
2025-03-02 18:14:37 +0000

    python stuff

--- symnmf.py
+++ symnmf.py
@@ -1,2 +1,12 @@
 import numpy as np
-Set np.random.seed(1234)
+np.random.seed(1234)
+
+def initialize_H(W, n, k):
+    m = np.mean(W)
+    H = np.random.uniform(0, 2 * ((m/k)**0.5), size=(n, k))
+
+def update_H(W, H, n, k):
+    halfs_mat = np.full((n, k), 0.5)
+    numerator = W@H
+    denominator = H@H.T@H
+    H = H*(0.5 + 0.5*numerator/denominator)  # notice that there isn't a check for dividing by 0 
