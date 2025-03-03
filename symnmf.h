fad79aba779f31b5690e28ef2101ad2396149082
Amit Shalvi Kali <a.shalvikali@gmail.com>
2025-03-03 13:29:56 +0200

    implemented sym in C API

--- symnmf.h
+++ symnmf.h
@@ -1,3 +1,6 @@
-double** sym(double** datapoint_coords, int N, int d);
-double** ddg(double** datapoint_coords, int N, int d);
-double** norm(double** datapoint_coords, int N, int d);
+const int SUCCESS = 0;
+const int ERROR = 1;
+
+int sym_C(double** datapoint_coords, int N, int d, double ***result);
+int ddg_C(double **datapoint_coords, int N, int d, double ***result);
+int norm_C(double** datapoint_coords, int N, int d, double ***result);
--- symnmfmodule.c
+++ symnmfmodule.c
@@ -1,15 +1,18 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
+#include "symnmf.h"
+
+#define GOTO_CLEANUP_IF_NULL(x) { if (x == NULL) { goto cleanup; } }
+#define GOTO_CLEANUP_IF_ERROR(x) { if ((x) == ERROR) { goto cleanup; } }
+#define GOTO_CLEANUP_IF_NEGATIVE(x) { if (x < 0) { goto cleanup; } }
 #define GOTO_CLEANUP_IF_PYERROR_OCCURED() { if (NULL != PyErr_Occurred()) { goto cleanup; } }
 
-static PyObject* symnmf(PyObject *self, PyObject *args);
 static PyObject* sym(PyObject *self, PyObject *args);
 static PyObject* ddg(PyObject *self, PyObject *args);
 static PyObject* norm(PyObject *self, PyObject *args);
 
 static PyMethodDef symnmfMethods[] = {
-    {"symnmf", (PyCFunction)symnmf, METH_VARARGS, PyDoc_STR("symnmf")},
     {"sym", (PyCFunction)sym, METH_VARARGS, PyDoc_STR("sym")},
     {"ddg", (PyCFunction)ddg, METH_VARARGS, PyDoc_STR("ddg")},
     {"norm", (PyCFunction)norm, METH_VARARGS, PyDoc_STR("norm")},
@@ -31,4 +34,115 @@ PyMODINIT_FUNC PyInit_symnmfmodule(void) {
         return NULL;
     }
     return m;
+}
+
+void free_2D_array(double **array) {
+    if (array != NULL) {
+        free(array[0]);
+        free(array);
+        array = NULL;
+    }
+}
+
+int get_datapoints(int N, int d, PyObject *datapoints, double ***datapoints_array_out) {
+    int return_code = ERROR;
+    int i;
+    int j;
+    double *p = NULL;
+    double **a = NULL;
+
+    p = calloc(d * N, sizeof(double));
+    GOTO_CLEANUP_IF_NULL(p);
+    a = calloc(N, sizeof(double *));
+    GOTO_CLEANUP_IF_NULL(a);
+
+    for (i = 0; i < N; i++) {
+        a[i] = p + (i * d);
+        PyObject *curr_item = PyList_GetItem(datapoints, i);
+        GOTO_CLEANUP_IF_NULL(curr_item);
+        for (j = 0; j < d; j++) {
+            PyObject *curr_coor = PyList_GetItem(curr_item, j);
+            GOTO_CLEANUP_IF_NULL(curr_coor);
+            a[i][j] = PyFloat_AsDouble(curr_coor);
+            GOTO_CLEANUP_IF_PYERROR_OCCURED();
+        }
+    }
+
+    return_code = SUCCESS;
+    *datapoints_array_out = a;
+
+cleanup:
+    if (return_code == ERROR) {
+        /* try to free memory */
+        free_2D_array(a);
+    }
+    return return_code;
+}
+
+int parse_datapoints(PyObject *self, PyObject *args, int *d, int *N, double ***datapoints_array_out) {
+    int return_code = ERROR;
+    PyObject *curr_item = NULL;
+    PyObject *datapoints = NULL;
+
+    if (!PyArg_ParseTuple(args, "O", &datapoints)) {
+        goto cleanup;
+    }
+
+    *N = PyObject_Length(datapoints);
+    GOTO_CLEANUP_IF_NEGATIVE(*N);
+
+    curr_item = PyList_GetItem(datapoints, 0);
+    GOTO_CLEANUP_IF_NULL(curr_item);
+    *d = PyObject_Length(curr_item);
+    GOTO_CLEANUP_IF_NEGATIVE(*d);
+
+    GOTO_CLEANUP_IF_ERROR(get_datapoints(*N, *d, datapoints, datapoints_array_out));
+
+    return_code = SUCCESS;
+
+cleanup:
+    return return_code;
+}
+
+int build_output_matrix(int N, double **matrix, PyObject **result) {
+    int return_code = ERROR;
+    PyObject *output = NULL;
+    PyObject *curr_coor = NULL;
+    int i;
+    int j;
+
+    output = PyList_New(N);
+    GOTO_CLEANUP_IF_NULL(output);
+
+    for (i = 0; i < N; i++) {
+        PyObject *row = PyList_New(N);
+        GOTO_CLEANUP_IF_NULL(row);
+        for (j = 0; j < N; j++) {
+            curr_coor = PyFloat_FromDouble(matrix[i][j]);
+            GOTO_CLEANUP_IF_NULL(curr_coor);
+            GOTO_CLEANUP_IF_NEGATIVE(PyList_SetItem(row, j, curr_coor));
+        }
+        GOTO_CLEANUP_IF_NEGATIVE(PyList_SetItem(output, i, row));
+    }
+
+    return_code = SUCCESS;
+    *result = output;
+
+cleanup:
+    return return_code;
+}
+
+static PyObject* sym(PyObject *self, PyObject *args) {
+    int N;
+    int d;
+    double **datapoints = NULL;
+    double **result = NULL;
+    PyObject *matrix_result = NULL;  /* output value */
+
+    GOTO_CLEANUP_IF_ERROR(parse_datapoints(self, args, &d, &N, &datapoints));
+    GOTO_CLEANUP_IF_ERROR(sym_C(datapoints, N, d, &result));
+    GOTO_CLEANUP_IF_ERROR(build_output_matrix(N, result, &matrix_result));
+
+cleanup:
+    return matrix_result;
 }
