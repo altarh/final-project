db518bad0ec40f0ac715db9584a39144e7b34684
altar horowitz <altarh@mail.tau.ac.il>
2025-03-03 11:09:58 +0000

    added HW1 file and analysis.py

---
+++ analysis.py
@@ -0,0 +1,52 @@
+from numpy import inf
+from sklearn import metrics 
+import sys
+import pandas as pd
+import HW1
+import symnmf
+import numpy as np
+
+def load_datapoints(filename):
+    datapoints = pd.read_csv(filename, sep = ",", header=None)
+    lst = datapoints.values.tolist()
+    return lst
+
+def main():
+    K = int(sys.argv[1])
+    file = sys.argv[2]
+    EPSILON = 0.001
+    DEFAULT_ITER = 300
+
+    datapoints = load_datapoints(file)
+    N = len(datapoints)
+    d = len(datapoints[0])
+
+    centroids = HW1.run_kmeans(K, datapoints, HW1.init_centroids(K, datapoints), d, DEFAULT_ITER)
+    kmeans_labeling = [0 for i in range(N)]
+    min_dist = inf
+    for datapoint_idx in range(N):
+        min_dist = inf
+        for centroid_idx in range(len(centroids)):
+            curr_dist = HW1.euclidean_distance(datapoints[datapoint_idx], centroids[centroid_idx])
+            if curr_dist < min_dist:
+                min_dist = curr_dist
+                min_idx = centroid_idx
+        kmeans_labeling[datapoint_idx] = min_idx
+    
+    kmeans_silhouette_score = metrics.silhouette_score(datapoints, kmeans_labeling, metric='euclidean')
+
+    W = symnmf.norm(datapoints)
+    H = symnmf.initialize_H(W, N, K)
+    H = symnmf.symnmf(H, W)
+
+    symnmf_clustering = np.argmax(H, axis=1)
+    symnmf_silhouette_score = metrics.silhouette_score(datapoints, symnmf_clustering, metric='euclidean')
+
+    print("nmf: " + symnmf_silhouette_score)
+    print("kmeans: " + kmeans_silhouette_score)
+
+if __name__ == "__main__":
+    # try:
+    main()
+    # except Exception:
+    #     symnmf.print_error()
--- symnmf.py
+++ symnmf.py
@@ -11,23 +11,22 @@ def initialize_H(W, n, k):
     H = np.random.uniform(0, 2 * ((m/k)**0.5), size=(n, k))
     return H
 
-def update_H(W, H, n, k):
-    halfs_mat = np.full((n, k), 0.5)
+def update_H(W, H):
     numerator = W@H
     denominator = H@H.T@H
     H = H*(0.5 + 0.5*numerator/denominator)  # notice that there isn't a check for dividing by 0 
     return H
 
-def symnmf(H, W, n, k):
+def symnmf(H, W):
     epsilon = 0.0001
     prevH = H
-    H = update_H(W, H, n, k)
+    H = update_H(W, H)
     diff_sum = np.sum((H - prevH)**2)
     iteration  = 0
     while (diff_sum > epsilon and iter < 300):
         iteration  += 1
         prevH = H
-        H = update_H(W, H, n, k)
+        H = update_H(W, H)
         diff_sum = np.sum((H - prevH)**2)
     return H
 
